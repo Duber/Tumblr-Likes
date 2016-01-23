2fdc7ce31618af3e2f9ff9a201b7c3991ac164a3
duber <miguel@miguelcruz.net>
2016-01-23 12:00:27 +0100

    renamed files in build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-coffeescript-concat -o output.coffee scripts/UQC.coffee scripts/ContentHelper.coffee scripts/Likes.coffee
-coffee -o . output.coffee
-mv output.js assets/js/likes.js
-rm output.coffee
+coffeescript-concat -o likes.coffee scripts/UQC.coffee scripts/ContentHelper.coffee scripts/Likes.coffee
+coffee -o . likes.coffee
+mv likes.js assets/js/likes.js
+rm likes.coffee
