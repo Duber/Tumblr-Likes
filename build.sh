0a39d86b659b2234763af3dcf93a4a8eac64c760
duber <miguel@miguelcruz.net>
2016-08-06 18:52:23 +0200

    renamed scripts folder to src

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-./node_modules/coffeescript-concat/coffeescript-concat -o likes.coffee scripts/ContentContext.coffee scripts/ContentHelper.coffee scripts/Header.coffee scripts/Like.coffee scripts/Unlike.coffee scripts/Program.coffee
-coffee -o assets/js/ scripts/Login.coffee
+./node_modules/coffeescript-concat/coffeescript-concat -o likes.coffee src/ContentContext.coffee src/ContentHelper.coffee src/Header.coffee src/Like.coffee src/Unlike.coffee src/Program.coffee
+coffee -o assets/js/ src/login.coffee
 coffee -o assets/js/ likes.coffee
 rm likes.coffee
