d81f4d9ad44beac88c9d46e2237186e8fab97cf9
Miguel Cruz <miguel@miguelcruz.net>
2016-08-09 22:53:12 +0000

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 ./node_modules/coffeescript-concat/coffeescript-concat -o likes.coffee src/ContentContext.coffee src/ContentHelper.coffee src/Header.coffee src/Like.coffee src/Unlike.coffee src/Program.coffee
-coffee -o assets/js/ src/login.coffee
-coffee -o assets/js/ likes.coffee
+./node_modules/coffee-script/bin/coffee -o assets/js/ src/Login.coffee
+./node_modules/coffee-script/bin/coffee -o assets/js/ likes.coffee
 rm likes.coffee
